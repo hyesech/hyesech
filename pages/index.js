f74cfba0f20a4bcc48e218f730e6b8a030680b8c
hyesech <hyesech@gmail.com>
2021-01-24 21:36:50 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>HYESECH TECH BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://hyesech.dev">hyesech</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>
+            Everything begins with your stance. Remember.
+          </code>
         </p>
 
         <div className={styles.grid}>
@@ -51,15 +52,10 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p className={styles.description}>
+          <code className={styles.code}>hyesech</code>
+        </p>
       </footer>
     </div>
-  )
+  );
 }
